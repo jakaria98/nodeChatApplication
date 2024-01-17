51b3bf918b1f2eb34989d77c967812b566516f09
jakaria98 <jhossain439@gmail.com>
2024-01-17 00:35:26 -0330

    inbox done

--- router/inboxRouter.js
+++ router/inboxRouter.js
@@ -1,8 +1,33 @@
-const router = require('express').Router();
+// external imports
+const express = require("express");
 
-const { getInbox } = require('../controller/inboxController');
-const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse")
-const { checkLogin } = require('../middlewares/common/checkLogin');
+// internal imports
+const {
+  getInbox,
+  searchUser,
+  addConversation,
+  getMessages,
+  sendMessage,
+} = require("../controller/inboxController");
+const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const { checkLogin } = require("../middlewares/common/checkLogin");
+const attachmentUpload = require("../middlewares/inbox/attachmentUpload");
 
-router.get('/', decorateHtmlResponse("Inbox"), checkLogin, getInbox);
+const router = express.Router();
+
+// inbox page
+router.get("/", decorateHtmlResponse("Inbox"), checkLogin, getInbox);
+
+// search user for conversation
+router.post("/search", checkLogin, searchUser);
+
+// add conversation
+router.post("/conversation", checkLogin, addConversation);
+
+// get messages of a conversation
+router.get("/messages/:conversation_id", checkLogin, getMessages);
+
+// send message
+router.post("/message", checkLogin, attachmentUpload, sendMessage);
+
 module.exports = router;
